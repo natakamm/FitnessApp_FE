3ab9d9a4df9620a86e44bac063205cfd2d72f6a4
natakamm <natakamm1@googlemail.com>
2025-02-07 14:05:17 +0100

    changed states in context to manage them easier

--- src/context/CategoryContext.jsx
+++ src/context/CategoryContext.jsx
@@ -1,73 +1,101 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, createContext } from "react";
 
 export const CategoryContext = createContext();
 
 export default function CategoryContextProvider({ children }) {
   const URL_BASE = "https://fitnessapp-be.onrender.com/";
 
-  const [categories, setCategories] = useState([]);
-  const [categoriesError, setCategoriesError] = useState(null);
-  const [categoriesloading, setCategoriesLoading] = useState(false);
+  // Group states for fetching all categories instead of having many loading and error states for each of those
+  const [allCategoriesState, setAllCategoriesState] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
 
+  const [singleCategoryState, setSingleCategoryState] = useState({
+    data: {},
+    loading: false,
+    error: null,
+  });
+
+  //Fetch all categories
   const fetchAllCategories = async () => {
-    setCategoriesError(null);
-    setCategoriesLoading(true);
+    setAllCategoriesState({ data: [], loading: true, error: null });
 
     try {
-      const res = await fetch(`${URL_BASE}category`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const res = await fetch(`${URL_BASE}category`);
       const data = await res.json();
+
       if (!res.ok) {
-        setCategoriesError(data.error || "Failed to fetch Categories.");
-        setCategoriesLoading(false);
-        return;
+        throw new Error(data.error || "Failed to fetch categories");
       }
-      setCategories(data.categories);
+
+      setAllCategoriesState({
+        data: data.categories,
+        loading: false,
+        error: null,
+      });
     } catch (error) {
-      setCategoriesError(
-        error.message || "An error has occurred while fetching categories."
-      );
-    } finally {
-      setCategoriesLoading(false);
+      setAllCategoriesState({ data: [], loading: false, error: error.message });
     }
   };
 
-  const [category, setCategory] = useState({});
-  const [categoryError, setCategoryError] = useState(null);
-  const [categoryLoading, setCategoryLoading] = useState(false);
-
+  // Fetch one category
   const fetchOneCategory = async (categoryId) => {
-    setCategoryError(null);
-    setCategoryLoading(true);
+    setSingleCategoryState({ data: {}, loading: true, error: null });
 
     try {
-      const res = await fetch(`${URL_BASE}category/${categoryId}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const res = await fetch(`${URL_BASE}category/${categoryId}`);
       const data = await res.json();
+
       if (!res.ok) {
-        setCategoryError(data.error);
-        setCategoryLoading(false);
-        return;
+        throw new Error(data.error || "Failed to fetch category");
       }
-      setCategory(data.category);
+
+      setSingleCategoryState({
+        data: data.category,
+        loading: false,
+        error: null,
+      });
     } catch (error) {
-      setCategoryError(
-        error.message || "An error occurred while fetching your category."
-      );
-    } finally {
-      setCategoryLoading(false);
+      setSingleCategoryState({
+        data: {},
+        loading: false,
+        error: error.message,
+      });
     }
   };
+
+  //USE LIKE THIS
+  // const {
+  //   allCategoriesState,
+  //   singleCategoryState,
+  //   fetchAllCategories,
+  //   fetchOneCategory,
+  // } = useContext(CategoryContext);
+
+  // useEffect(() => {
+  //   fetchAllCategories();
+  //   fetchOneCategory("679b611adb090ce990b1a5f3");
+  // }, []);
+
+  // useEffect(() => {
+  //   if (singleCategoryState.data) {
+  //     console.log("ONE:", singleCategoryState.data);
+  //   }
+  //   if (allCategoriesState.data.length > 0) {
+  //     console.log("ALL:", allCategoriesState.data);
+  //   }
+  // }, [singleCategoryState.data, allCategoriesState.data]);
 
   return (
     <CategoryContext.Provider
-      value={{ fetchAllCategories, categories, fetchOneCategory, category }}
+      value={{
+        allCategoriesState,
+        singleCategoryState,
+        fetchAllCategories,
+        fetchOneCategory,
+      }}
     >
       {children}
     </CategoryContext.Provider>
