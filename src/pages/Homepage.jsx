69616630715d1998ee287f0d4877b4b7dc730717
natakamm <natakamm1@googlemail.com>
2025-02-17 12:34:25 +0100

    added scroll progressbar element

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import Hero from "../assets/components/Hero";
 import AnimateText from "../assets/components/AnimateText";
 import CatsCarousel from "../assets/components/CatsCarousel";
+import Scrollbar from "../assets/components/Scrollbar";
 const Homepage = () => {
   return (
     <div>
       <Hero />
       <AnimateText />
+      <Scrollbar />
       <CatsCarousel />
     </div>
   );
