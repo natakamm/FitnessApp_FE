5fa095d0996a9ee655795c5746a74370c8d2e6f4
natakamm <natakamm1@googlemail.com>
2025-02-24 14:49:28 +0100

    created button with hover effect

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,12 +3,14 @@ import AnimateText from "../assets/components/AnimateText";
 import CatsCarousel from "../assets/components/CatsCarousel";
 import CoursesList from "../assets/components/CoursesList";
 import Scrollbar from "../assets/components/Scrollbar";
+import FindMoreButton from "../assets/components/FindMoreButton";
 const Homepage = () => {
   return (
     <div>
       <Hero />
       <AnimateText />
       <Scrollbar />
+      <FindMoreButton text="Find More" />
       <CatsCarousel />
       <CoursesList />
     </div>
